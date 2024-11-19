60ce54f059492cb324e5756e117f286dd18f777d
PriyaBalaDharshini <priyaarasu12@gmail.com>
2024-11-19 07:00:24 +0530

    MVC patter added

--- Backend/index.js
+++ Backend/index.js
@@ -1,55 +1,16 @@
 import express from "express";
 import cors from "cors";
-import axios from "axios";
-import * as cheerio from 'cheerio';
+import productRoute from "./routes/productRoute.js"
+import dotenv from "dotenv"
+dotenv.config()
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 app.use(cors())
 
-app.get("/", (req, res) => {
-    res.status(200).send("Backend")
-})
+app.use("/", productRoute)
 
-app.get("/search", async (req, res) => {
-    const searchQuery = req.query.searchQuery;
-
-    try {
-        const url = `https://www.amazon.in/s?k=${encodeURIComponent(searchQuery)}`;
-        const { data } = await axios.get(url, {
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-            }
-        });
-        console.log(data);
-
-        const $ = cheerio.load(data);
-        const products = []
-
-        $('.s-result-item').each((i, ele) => {
-            const productName = $(ele).find('h2 a span').text().trim();
-            const price = $(ele).find('.a-price-whole').text().trim();
-            const rating = $(ele).find('.a-icon-alt').text().trim();
-            const asin = $(ele).attr('data-asin');
-
-            if (productName && asin) {
-                products.push({
-                    productName,
-                    price,
-                    rating: rating || "N/A",
-                    asin
-                })
-            }
-        })
-        res.send({ products })
-
-    } catch (error) {
-        console.log('Error getting data:', error.message);
-        res.send(error)
-    }
-
-})
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
